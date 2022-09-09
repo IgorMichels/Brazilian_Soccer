4633512e857c593b304e0fb2fb65752bfa7f17a6
IgorMichels <igorxadrezbr@gmail.com>
2022-09-08 22:24:51 -0300

    added scipy optimize

--- Code/Python/frequentist_approach.py
+++ Code/Python/frequentist_approach.py
@@ -1,5 +1,6 @@
 from numpy.random import poisson as rv_poisson
 from numpy.random import multivariate_normal
+from scipy.optimize import minimize
 from scipy.stats import poisson
 from glob import glob
 
@@ -45,12 +46,15 @@ def games_results(lamb_1 = 1, lamb_2 = 1, lamb_3 = 0, size = 100000):
     return R, (w, d, l)
 
 def game_likelihood(lambs, goals):
-    lik_score_1 = poisson.logcdf(goals[0], lambs[0] / lambs[1])
-    lik_score_2 = poisson.logcdf(goals[1], lambs[1] / lambs[0])
+    lik_score_1 = poisson.logcdf(goals[0], lambs[0])
+    lik_score_2 = poisson.logcdf(goals[1], lambs[1])
     
     return lik_score_1 + lik_score_2
 
 def likelihood(proficiencies, players, squads):
+    if proficiencies.shape[0] != len(players):
+        proficiencies = proficiencies.reshape(len(players), 2)
+    
     lik = 0
     for game in squads:
         for sub in squads[game]:
@@ -58,35 +62,55 @@ def likelihood(proficiencies, players, squads):
             lamb_21, lamb_22 = 0, 0
             goals = squads[game][sub]['Placar']
             t = squads[game][sub]['Tempo']
-            for i in range(11):
-                player = squads[game][sub]['Mandante'][i]
-                lamb_11 += proficiencies[players[player], 0]
-                lamb_12 += proficiencies[players[player], 1]
+            if t != 0:
+                for i in range(11):
+                    player = squads[game][sub]['Mandante'][i]
+                    lamb_11 += proficiencies[players[player], 0]
+                    lamb_12 += proficiencies[players[player], 1]
                 
-                player = squads[game][sub]['Visitante'][i]
-                lamb_21 += proficiencies[players[player], 0]
-                lamb_22 += proficiencies[players[player], 1]
+                    player = squads[game][sub]['Visitante'][i]
+                    lamb_21 += proficiencies[players[player], 0]
+                    lamb_22 += proficiencies[players[player], 1]
             
+                lamb_11 *= t / 90
+                lamb_12 *= t / 90
+                lamb_21 *= t / 90
+                lamb_22 *= t / 90
+                lambs = [lamb_11 / lamb_22, lamb_21 / lamb_12]
+                lik += game_likelihood(lambs, goals)
             
-            lamb_11 *= t / 90
-            lamb_12 *= t / 90
-            lamb_21 *= t / 90
-            lamb_22 *= t / 90
-            
-            lambs = [lamb_11 / lamb_22, lamb_21 / lamb_12]
-            lik += game_likelihood(lambs, goals)
+    return - lik
 
-    return lik
+with open('../../Scrape/Serie_A/2022/squads.json', 'r') as f:
+    squads = json.load(f)
+    
+i = 0
+players = {}
+for game in squads:
+    for sub in squads[game]:
+        for player in squads[game][sub]['Mandante']:
+            if player not in players:
+                players[player] = i
+                i += 1
+                
+        for player in squads[game][sub]['Visitante']:
+            if player not in players:
+                players[player] = i
+                i += 1
 
-# mu1, mu2 = 1, 2
-# x, y, z = 2, 1, 2
-# print(multivariate_normal([mu1, mu2], [[x, y], [y, z]], 100000))
+mu1, mu2 = 1, 2
+x, y, z = 2, 1, 2
+proficiencies = np.abs(multivariate_normal([mu1, mu2], [[x, y], [y, z]], i)).reshape(2 * len(players), 1)
+print(likelihood(proficiencies, players, squads))
+res = minimize(likelihood, proficiencies, args = (players, squads))
+print(res.fun)
+print(res.nit)
 
-n = 1000000
+#n = 1000000
 # probs = games_probs(lamb_1 = 2, lamb_2 = 1, lamb_3 = 0, size = n)
-results, probs = games_results(lamb_1 = 2, lamb_2 = 1, lamb_3 = 0, size = n)
+#results, probs = games_results(lamb_1 = 2, lamb_2 = 1, lamb_3 = 0, size = n)
 
-print(probs)
-print(game_likelihood([2, 1], [3, 2]))
-print(game_likelihood([3, 2], [3, 2]))
+#print(probs)
+#print(game_likelihood([2, 1], [3, 2]))
+#print(game_likelihood([3, 2], [3, 2]))
 
