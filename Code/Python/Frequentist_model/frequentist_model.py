bec2f5a696c496fd7f883831d4103cfa9419aa91
IgorMichels <igorxadrezbr@gmail.com>
2022-10-08 15:16:47 -0300

    cleaning model

--- Code/Python/Frequentist_model/frequentist_model.py
+++ Code/Python/Frequentist_model/frequentist_model.py
@@ -1,50 +1,9 @@
-from scipy.optimize import minimize, LinearConstraint
-from numpy.random import poisson as rv_poisson
-from numpy.random import multivariate_normal
-from scipy.stats import poisson
-from math import factorial
-from glob import glob
-
-import os
 import json
 import numpy as np
-import matplotlib.pyplot as plt
 
-def bivariate_poisson(lamb_1 = 1, lamb_2 = 1, lamb_3 = 0, size = 1):
-    X_1 = rv_poisson(lamb_1, size)
-    X_2 = rv_poisson(lamb_2, size)
-    X_3 = rv_poisson(lamb_3, size)
-    Y_1 = X_1 + X_3
-    Y_2 = X_2 + X_3
-    
-    return np.vstack([Y_1, Y_2]).T
-
-def is_positive_semidefinite(x, y, z):
-    t1 = x + z - (x**2 - 2 * x * z + 4 * y**2 + z**2)**(1/2) >= 0
-    t2 = x + z + (x**2 - 2 * x * z + 4 * y**2 + z**2)**(1/2) >= 0
-    
-    return t1 * t2
-
-def games_probs(lamb_1 = 1, lamb_2 = 1, lamb_3 = 0, size = 100000):
-    sims = bivariate_poisson(lamb_1, lamb_2, lamb_3, size)
-    w = round(sum(np.greater(sims[:, 0], sims[:, 1])) / size, 4)
-    l = round(sum(np.less(sims[:, 0], sims[:, 1])) / size, 4)
-    d = round(1 - w - l, 4)
-
-    return w, d, l
-    
-def games_results(lamb_1 = 1, lamb_2 = 1, lamb_3 = 0, size = 100000):
-    sims = bivariate_poisson(lamb_1, lamb_2, lamb_3, size)
-    R = np.zeros(np.max(sims, 0) + 1)
-    for sim in sims:
-        R[sim[0], sim[1]] += 1
-    
-    R = R / size
-    w = round(np.sum(np.tril(R, -1)), 6)
-    d = round(np.sum(np.diag(R)), 6)
-    l = round(np.sum(np.triu(R, 1)), 6)
-    
-    return R, (w, d, l)
+from copy import deepcopy
+from scipy.stats import poisson
+from scipy.optimize import minimize, fmin
 
 def calculate_lambs(subgame, players, proficiencies):
     n = len(players)
@@ -154,44 +113,64 @@ def likelihood_hessian(proficiencies, players, squads):
                         
     return hessian
 
-with open('../../../Scrape/Serie_A/2021/squads.json', 'r') as f:
-    squads = json.load(f)
-    
-i = 0
-players = {}
-for game in squads:
-    for sub in squads[game]:
-        for player in squads[game][sub]['Mandante']:
-            if player not in players:
-                players[player] = i
-                i += 1
+def collect_data(years, competitions):
+    for competition in competitions:
+        for year in years:
+            with open(f'../../../Scrape/{competition}/{year}/squads.json', 'r') as f:
+                squads = json.load(f)
                 
-        for player in squads[game][sub]['Visitante']:
-            if player not in players:
-                players[player] = i
-                i += 1
+            i = 0
+            players = {}
+            game_id = 0
+            new_squads = {}
+            for game in squads:
+                game_id += 1
+                new_squads[str(game_id).zfill(5)] = deepcopy(squads[game])
+                for sub in squads[game]:
+                    for player in squads[game][sub]['Mandante']:
+                        if player not in players:
+                            players[player] = i
+                            i += 1
+                    
+                    for player in squads[game][sub]['Visitante']:
+                        if player not in players:
+                            players[player] = i
+                            i += 1
+                    
+    return players, new_squads
 
-mu = 0
-sigma = 3
-proficiencies = np.abs(np.random.normal(mu, sigma, 2 * len(players)))
-proficiencies[0] = 1
-print(proficiencies)
-res = minimize(
-               likelihood,
-               proficiencies,
-               args = (players, squads),
-               method = 'trust-exact',
-               # method = 'Newton-CG',
-               jac = likelihood_gradient,
-               hess = likelihood_hessian,
-               tol = 1e-20,
-               # options = {
-               #            'xtol': 1e-20,
-               #            'eps' : 1e-10
-               #           }
-              )
+if __name__ == '__main__':
+    competitions = ['Serie_A', 'Serie_B']
+    years = range(2020, 2022)
+    players, squads = collect_data(years, competitions)
+    mu = 0
+    sigma = 3
+    proficiencies = np.abs(np.random.normal(mu, sigma, 2 * len(players)))
+    proficiencies[0] = 1
+    print(proficiencies)
+    res = minimize(
+                   likelihood,
+                   proficiencies,
+                   args = (players, squads),
+                   method = 'trust-exact',
+                   # method = 'Newton-CG',
+                   jac = likelihood_gradient,
+                   hess = likelihood_hessian,
+                   tol = 1e-20,
+                   # options = {
+                   #            'xtol': 1e-20,
+                   #            'eps' : 1e-10
+                   #           }
+                  )
+                  
+    #res = fmin(
+    #           likelihood,
+    #           proficiencies,
+    #           args = (players, squads),
+    #          )
 
-np.save('result_trust_exact.npy', res.x)
-print(res.x)
-print(res.fun)
-print(res)
+    #np.save('result_fmin.npy', res.x)
+    np.save('result_trust_exact.npy', res.x)
+    print(res.x)
+    print(res.fun)
+    print(res)
