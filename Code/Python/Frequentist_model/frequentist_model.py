8dbed05e75b200825644336a79956e6f9df723ef
IgorMichels <igorxadrezbr@gmail.com>
2022-10-15 12:37:23 -0300

    fix functions

--- Code/Python/Frequentist_model/frequentist_model.py
+++ Code/Python/Frequentist_model/frequentist_model.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from copy import deepcopy
 from scipy.stats import poisson
+from numpy.linalg import norm
 from scipy.optimize import minimize, fmin
 
 def calculate_lambs(subgame, players, proficiencies):
@@ -45,7 +46,7 @@ def likelihood(proficiencies, players, squads):
                 lambs = [lamb_1, lamb_2]
                 lik += game_likelihood(lambs, goals)
                 
-    return lik
+    return - lik
 
 def likelihood_gradient(proficiencies, players, squads):
     n = len(players)
@@ -68,7 +69,7 @@ def likelihood_gradient(proficiencies, players, squads):
                     gradient[player    ] += - t / lamb_md + s_v / lamb_va
                     gradient[player + n] += t / lamb_vd * lamb_ma / lamb_vd - s_m / lamb_vd
                 
-    return gradient
+    return - gradient
 
 def likelihood_hessian(proficiencies, players, squads):
     n = len(players)
@@ -111,66 +112,97 @@ def likelihood_hessian(proficiencies, players, squads):
                         hessian[player_i + n, player_j    ] += 0 # def/atk
                         hessian[player_i + n, player_j + n] += s_m / (lamb_vd ** 2) - 2 * t * lamb_ma / (lamb_vd ** 3) # def/def
                         
-    return hessian
+    return - hessian
 
-def collect_data(years, competitions):
+def collect_data(years, competitions, players_file):
+    with open(players_file, 'r') as f:
+        players = json.load(f)
+    
+    for player in players:
+        players[player] -= 1
+    
+    game_id = 0
+    new_squads = {}
     for competition in competitions:
         for year in years:
             with open(f'../../../Scrape/{competition}/{year}/squads.json', 'r') as f:
                 squads = json.load(f)
-                
-            i = 0
-            players = {}
-            game_id = 0
-            new_squads = {}
+            
             for game in squads:
                 game_id += 1
                 new_squads[str(game_id).zfill(5)] = deepcopy(squads[game])
-                for sub in squads[game]:
-                    for player in squads[game][sub]['Mandante']:
-                        if player not in players:
-                            players[player] = i
-                            i += 1
-                    
-                    for player in squads[game][sub]['Visitante']:
-                        if player not in players:
-                            players[player] = i
-                            i += 1
+                #for sub in squads[game]:
+                #    if squads[game][sub]['Tempo'] != 0:
+                #        new_squads[str(game_id).zfill(5)][sub]['Mandante'] = []
+                #        for player in squads[game][sub]['Mandante']:
+                #            new_squads[str(game_id).zfill(5)][sub]['Mandante'].append(players[player])
+                #    
+                #        new_squads[str(game_id).zfill(5)][sub]['Visitante'] = []
+                #        for player in squads[game][sub]['Visitante']:
+                #            new_squads[str(game_id).zfill(5)][sub]['Visitante'].append(players[player])
                     
     return players, new_squads
+
+def optimizer(proficiencies, players, squads, max_iter = 10000, eps = 1e-6, tol = 1e-8):
+    fx_ant = np.inf
+    fx = likelihood(proficiencies, players, squads)
+    gx = likelihood_gradient(proficiencies, players, squads)
+    n_iter = 0
+    while n_iter < max_iter and norm(gx) > eps and abs(fx_ant - fx) > tol:
+        n_iter += 1
+        t = 1
+        while fx < likelihood(proficiencies - t * gx, players, squads):
+            t = t / 1.5
+            
+        proficiencies -= t * gx
+        fx_ant = fx
+        fx = likelihood(proficiencies, players, squads)
+        gx = likelihood_gradient(proficiencies, players, squads)
+        print(n_iter, fx, norm(gx), t)
+        
+    return proficiencies, n_iter, fx, gx
 
 if __name__ == '__main__':
     competitions = ['Serie_A', 'Serie_B']
     years = range(2020, 2022)
-    players, squads = collect_data(years, competitions)
+    players_file = f'../Commons/players_{str(years[0])[-2:]}{competitions[-1][-1]}_all.json'
+    players, squads = collect_data(years, competitions, players_file)
     mu = 0
     sigma = 3
     proficiencies = np.abs(np.random.normal(mu, sigma, 2 * len(players)))
     proficiencies[0] = 1
-    print(proficiencies)
-    #res = minimize(
-    #               likelihood,
-    #               proficiencies,
-    #               args = (players, squads),
-    #               # method = 'trust-exact',
-    #               method = 'Newton-CG',
-    #               jac = likelihood_gradient,
-    #               # hess = likelihood_hessian,
-    #               tol = 1e-20,
-    #               # options = {
-    #               #            'xtol': 1e-20,
-    #               #            'eps' : 1e-10
-    #               #           }
-    #              )
+    res = minimize(
+                   likelihood,
+                   proficiencies,
+                   args = (players, squads),
+                   method = 'trust-exact',
+                   # method = 'Newton-CG',
+                   jac = likelihood_gradient,
+                   hess = likelihood_hessian,
+                   tol = 1e-20,
+                   #options = {
+                   #           'xtol': 1e-20,
+                   #           'eps' : 1e-10
+                   #          }
+                  )
                   
-    res = fmin(
-               likelihood,
-               proficiencies,
-               args = (players, squads),
-              )
+    # res = fmin(
+    #            likelihood,
+    #            proficiencies,
+    #            args = (players, squads),
+    #           )
 
-    np.save('result_fmin.npy', res.x)
-    #np.save('result_trust_exact.npy', res.x)
+    #np.save('result_fmin.npy', res.x)
+    np.save('result_trust_exact.npy', res.x)
+    #np.save('newton_cg.npy', res.x)
+    print(proficiencies)
     print(res.x)
     print(res.fun)
     print(res)
+    
+    #proficiencies_opt, n_iter, fx, gx = optimizer(deepcopy(proficiencies), players, squads, max_iter = 10000, eps = 1e-6)
+    #proficiencies_opt = proficiencies_opt / proficiencies_opt[0]
+    #print()
+    #print(norm(proficiencies - proficiencies_opt))
+    #print(proficiencies)
+    #print(proficiencies_opt)
